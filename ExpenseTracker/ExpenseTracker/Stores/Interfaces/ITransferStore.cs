0daeb26ca5748d98403a0c8958adabd098b0102f
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-16 00:08:33 +0500

    add ITransferStore interface

---
+++ ExpenseTracker/ExpenseTracker/Stores/Interfaces/ITransferStore.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTracker.Domain.Entities;
+using ExpenseTracker.ViewModels.Transfer;
+
+namespace ExpenseTracker.Stores.Interfaces
+{
+    public interface ITransferStore
+    {
+        List<TransferViewModel> GetAll(string? search);
+        TransferViewModel GetById(int id);
+        TransferViewModel Create(CreateTransferViewModel transfer);
+        void Update(TransferViewModel transfer);
+        void Delete(int id);
+    }
+}
