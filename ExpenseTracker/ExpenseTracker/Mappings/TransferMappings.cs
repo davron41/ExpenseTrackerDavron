432f4a6713bf88848dea9d0b7c2f8e13bc13d957
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-16 00:08:10 +0500

    add transferMappings

---
+++ ExpenseTracker/ExpenseTracker/Mappings/TransferMappings.cs
@@ -0,0 +1,41 @@
+﻿using ExpenseTracker.Domain.Entities;
+using ExpenseTracker.ViewModels.Transfer;
+
+namespace ExpenseTracker.Mappings
+{
+    public static class TransferMappings
+    {
+        public static TransferViewModel ToViewModel(this Transfer transfer)
+        {
+            return new TransferViewModel
+            {
+                Amount = transfer.Amount,
+                Description = transfer.Description,
+                Id = transfer.Id,
+                Title = transfer.Title,
+                Type = transfer.Type,
+            };
+        }
+        public static Transfer ToEntity(this CreateTransferViewModel transfer)
+        {
+            return new Transfer
+            {
+                Amount = transfer.Amount,
+                Description = transfer.Description,
+                Title = transfer.Title,
+                Type = transfer.Type,
+            };
+        }
+        public static Transfer ToEntity(this TransferViewModel transfer)
+        {
+            return new Transfer
+            {
+                Amount = transfer.Amount,
+                Description = transfer.Description,
+                Title = transfer.Title,
+                Type = transfer.Type,
+                Id = transfer.Id,
+            };
+        }
+    }
+}
