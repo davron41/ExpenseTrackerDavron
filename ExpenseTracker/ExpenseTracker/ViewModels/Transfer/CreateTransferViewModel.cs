e0ec106aa2e1379970847c583b113d4f572925b7
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-16 00:09:09 +0500

    ViewModels for Transfers

---
+++ ExpenseTracker/ExpenseTracker/ViewModels/Transfer/CreateTransferViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTracker.Domain.Enums;
+
+namespace ExpenseTracker.ViewModels.Transfer
+{
+    public class CreateTransferViewModel
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public decimal Amount { get; set; }
+        public TransferType Type { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker/ViewModels/Transfer/TransferViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTracker.Domain.Enums;
+
+namespace ExpenseTracker.ViewModels.Transfer
+{
+    public class TransferViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }=string.Empty;
+        public string? Description { get; set; }
+        public decimal Amount { get; set; }
+        public TransferType Type { get; set; }
+    }
+}
