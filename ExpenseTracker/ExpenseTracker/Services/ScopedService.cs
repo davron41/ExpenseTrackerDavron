5e53f10364e5402708387c9e33a8279594d4285d
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-15 15:45:23 +0500

    services

---
+++ ExpenseTracker/ExpenseTracker/Services/ScopedService.cs
@@ -0,0 +1,25 @@
+﻿namespace ExpenseTracker.Services
+{
+    public interface IScopedService
+    {
+        string GetValues();
+        void SetValues(string value);
+    }
+    public class ScopedService : IScopedService
+    {
+        private string _value = string.Empty;
+        public ScopedService()
+        {
+            _value = "Scoped Service";
+        }
+        public void SetValues(string value)
+        {
+            _value = value;
+        }
+
+        public string GetValues()
+        {
+            return $"scoped : {_value}";
+        }
+    }
+}
