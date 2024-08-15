b12119bf209a4abe66d5abd9bd75723c301e4e05
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-15 15:45:32 +0500

    controllers

--- ExpenseTracker/ExpenseTracker/Controllers/HomeController.cs
+++ ExpenseTracker/ExpenseTracker/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using ExpenseTracker.Models;
+using ExpenseTracker.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,14 +8,28 @@ namespace ExpenseTracker.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ISmsService _smsService;
+        private readonly IEmailService _emailService;
+        public HomeController(ILogger<HomeController> logger,
+            ISmsService smsService,
+            IEmailService emailService)
         {
             _logger = logger;
+            _smsService = smsService;
+            _emailService = emailService;
         }
 
         public IActionResult Index()
         {
+            ViewBag.SmsValue = _smsService.GetValues();
+            ViewBag.EmailValue = _emailService.GetValues();
+
+            _smsService.ChangeValues();
+            _emailService.ChangeValues();
+
+            ViewBag.UpdatedSmsValue = _smsService.GetValues();
+            ViewBag.UpdatedEmailValue = _emailService.GetValues();
+
             return View();
         }
 
