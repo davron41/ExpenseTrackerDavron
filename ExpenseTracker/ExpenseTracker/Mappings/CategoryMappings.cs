0b0b5398d1078b23b4d69c8dfecbd83f355b62ef
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-15 22:18:56 +0500

    mappings folder

---
+++ ExpenseTracker/ExpenseTracker/Mappings/CategoryMappings.cs
@@ -0,0 +1,48 @@
+﻿using ExpenseTracker.Domain.Entities;
+using ExpenseTracker.ViewModels.Category;
+
+namespace ExpenseTracker.Mappings;
+
+public static class CategoryMappings
+{
+    public static CategoryViewModel ToViewModel(this Category category)
+    {
+        return new CategoryViewModel
+        {
+            Id = category.Id,
+            Name = category.Name,
+            Description = category.Description,
+            CreatedAt = category.CreatedAt,
+            UpdatedAt = category.UpdatedAt,
+        };
+    }
+
+    public static UpdateCategoryViewModel ToViewModel(this CategoryViewModel category)
+    {
+        return new UpdateCategoryViewModel
+        {
+            Id = category.Id,
+            Name = category.Name,
+            Description = category.Description
+        };
+    }
+
+    public static Category ToEntity(this CreateCategoryViewModel category)
+    {
+        return new Category
+        {
+            Name = category.Name,
+            Description = category.Description,
+        };
+    }
+
+    public static Category ToEntity(this UpdateCategoryViewModel category)
+    {
+        return new Category
+        {
+            Id = category.Id,
+            Name = category.Name,
+            Description = category.Description,
+        };
+    }
+}
