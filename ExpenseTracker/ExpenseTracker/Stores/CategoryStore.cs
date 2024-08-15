3f748fd8174c0f410fc6b139825a66a4a4bd39bc
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-15 15:45:41 +0500

    Stores folder

---
+++ ExpenseTracker/ExpenseTracker/Stores/CategoryStore.cs
@@ -0,0 +1,29 @@
+﻿using ExpenseTracker.Domain.Entities;
+using ExpenseTracker.Domain.Interfaces;
+
+namespace ExpenseTracker.Stores
+{
+    public interface ICategoryStore
+    {
+        void Add(Category category);
+    }
+    public class CategoryStore : ICategoryStore
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryStore(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public void Add(Category category)
+        {
+            _categoryRepository.Create(category);
+        }
+    }
+    public class NewCategoryStore : ICategoryStore
+    {
+        public void Add(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
