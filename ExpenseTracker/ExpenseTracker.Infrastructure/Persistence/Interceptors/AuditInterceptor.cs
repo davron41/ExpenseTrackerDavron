69e2b7505b61d98caf6c63e212e22d5f6d0075fa
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-17 23:26:35 +0500

    added intercepror folder

---
+++ ExpenseTracker/ExpenseTracker.Infrastructure/Persistence/Interceptors/AuditInterceptor.cs
@@ -0,0 +1,51 @@
+﻿using ExpenseTracker.Domain.Common;
+using ExpenseTracker.Infrastructure.Persistence.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace ExpenseTracker.Infrastructure.Persistence.Interceptors;
+
+internal class AuditInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        if (eventData.Context is not null)
+        {
+            UpdateEntities(eventData.Context);
+        }
+
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        if (eventData.Context is not null)
+        {
+            UpdateEntities(eventData.Context);
+        }
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private static void UpdateEntities(DbContext context)
+    {
+        if (context is not ExpenseTrackerDbContext dbContext)
+        {           
+            return;
+        }
+        
+        foreach(var entry in context.ChangeTracker.Entries<AuditableEntity>())
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+            }
+
+            if (entry.State == EntityState.Modified || entry.HasChangedOwnedEntities())
+            {
+                entry.Property(x => x.CreatedAt).IsModified = false;
+                entry.Entity.UpdatedAt = DateTime.UtcNow;
+            }
+        }
+    }
+}
