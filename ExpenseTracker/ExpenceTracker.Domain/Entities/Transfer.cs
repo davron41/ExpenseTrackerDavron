9aeb8b7baf80477b7bfd44017faa1003fc67032c
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-20 20:04:26 +0500

    transfer entity changes

--- ExpenseTracker/ExpenceTracker.Domain/Entities/Transfer.cs
+++ ExpenseTracker/ExpenceTracker.Domain/Entities/Transfer.cs
@@ -6,6 +6,7 @@ public class Transfer : AuditableEntity
 {
     public string? Note { get; set; }
     public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
 
     public int CategoryId { get; set; }
     public virtual Category Category { get; set; }
