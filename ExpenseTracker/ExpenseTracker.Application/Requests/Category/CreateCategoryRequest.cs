b2109f27475c709ea0b8d76d21057e47cd18c4a7
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-09-22 15:35:14 +0500

    categories request folder

--- ExpenseTracker/ExpenseTracker.Application/Requests/Category/CreateCategoryRequest.cs
+++ ExpenseTracker/ExpenseTracker.Application/Requests/Category/CreateCategoryRequest.cs
@@ -6,6 +6,6 @@ namespace ExpenseTracker.Application.Requests.Category;
 public class CreateCategoryRequest : UserRequest
 {
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public CategoryType Type { get; set; }
 }
