bd1b47781ef7efa1b7405e92830e9f340bb2da86
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-09-29 15:11:30 +0500

    Implement wallet controller endpoints

--- ExpenseTracker/ExpenseTracker/Controllers/WalletsController.cs
+++ ExpenseTracker/ExpenseTracker/Controllers/WalletsController.cs
@@ -1,12 +1,100 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpenseTracker.Application.Requests.Wallet;
+using ExpenseTracker.Application.Stores.Interfaces;
+using ExpenseTracker.Application.ViewModels.Wallet;
+using Microsoft.AspNetCore.Mvc;
 
-namespace ExpenseTracker.Controllers
+namespace ExpenseTracker.Controllers;
+
+public class WalletsController : Controller
 {
-    public class WalletsController : Controller
+    private readonly IWalletStore _store;
+
+    public WalletsController(IWalletStore store)
     {
-        public IActionResult Index()
+        _store = store ?? throw new ArgumentNullException(nameof(store));
+    }
+
+    public IActionResult Index([FromQuery] GetWalletsRequest request)
+    {
+        var wallets = _store.GetAll(request);
+
+        return View(wallets);
+    }
+
+    public IActionResult Details([FromRoute] WalletRequest request)
+    {
+        var wallet = _store.GetById(request);
+
+        return View(wallet);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create([FromForm] CreateWalletRequest request)
+    {
+        if (!ModelState.IsValid)
         {
-            return View();
+            return View(request);
         }
+
+        var createdWallet = _store.Create(request);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Edit([FromRoute] WalletRequest request)
+    {
+        var wallet = _store.GetById(request);
+
+        return View(wallet);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit([FromForm] UpdateWalletRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(request);
+        }
+
+        _store.Update(request);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Delete([FromRoute] WalletRequest request)
+    {
+        var wallet = _store.GetById(request);
+
+        return View(wallet);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Route("Delete")]
+    public IActionResult DeleteConfirmed([FromRoute] WalletRequest request)
+    {
+        _store.Delete(request);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    /// <summary>
+    /// Filters wallets
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns>List of filtered wallets</returns>
+    [Route("getWallets")]
+    public ActionResult<WalletViewModel> GetWallets([FromQuery] GetWalletsRequest request)
+    {
+        var wallets = _store.GetAll(request);
+
+        return Ok(wallets);
     }
 }
