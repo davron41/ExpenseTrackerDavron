cc5cb07c8ee8ab6f284937ca913b51418e8c6aff
begzhanmaxamatxanov <begzhan.maxamatxanov@gmail.com>
2024-10-24 01:54:19 +0500

    Update Application.DependencyInjection

--- ExpenseTracker/ExpenseTracker.Application/Extensions/DependencyInjection.cs
+++ ExpenseTracker/ExpenseTracker.Application/Extensions/DependencyInjection.cs
@@ -3,20 +3,18 @@ using ExpenseTracker.Application.Services;
 using ExpenseTracker.Application.Services.Interfaces;
 using ExpenseTracker.Application.Stores;
 using ExpenseTracker.Application.Stores.Interfaces;
-using ExpenseTracker.Stores;
-using ExpenseTracker.Stores.Interfaces;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ExpenseTracker.Application.Extensions;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection RegisterApplication(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection RegisterApplication(this IServiceCollection services)
     {
         services.AddScoped<ICategoryStore, CategoryStore>();
         services.AddScoped<ITransferStore, TransferStore>();
         services.AddScoped<IWalletStore, WalletStore>();
+        services.AddScoped<IUserStore, UserStore>();
         services.AddScoped<ICurrentUserService, CurrentUserService>();
 
         services.AddSignalR();
