8f88a48082e25b14be2f8a5f6f92e7416346094e
begzhanmaxamatxanov <begzhan.maxamatxanov@gmail.com>
2024-10-24 01:55:24 +0500

    correct namespace

--- ExpenseTracker/ExpenseTracker.Application/Mappings/CategoryMappings.cs
+++ ExpenseTracker/ExpenseTracker.Application/Mappings/CategoryMappings.cs
@@ -2,7 +2,7 @@
 using ExpenseTracker.Application.ViewModels.Category;
 using ExpenseTracker.Domain.Entities;
 
-namespace ExpenseTracker.Mappings;
+namespace ExpenseTracker.Application.Mappings;
 
 public static class CategoryMappings
 {
