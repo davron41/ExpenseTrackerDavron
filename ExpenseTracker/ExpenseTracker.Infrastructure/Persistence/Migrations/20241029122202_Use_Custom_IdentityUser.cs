b8576854d03609fdb5b0bcdd38deb38fcfe6b5a6
Davronbek <davronbek8733@gmail.com>
2024-10-29 17:27:49 +0500

    add database migration

---
+++ ExpenseTracker/ExpenseTracker.Infrastructure/Persistence/Migrations/20241029122202_Use_Custom_IdentityUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTracker.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Use_Custom_IdentityUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthdate",
+                table: "User",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthdate",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "User");
+        }
+    }
+}
